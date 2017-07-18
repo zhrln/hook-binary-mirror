4e3a713c1c1b99e9cc6c6696e996d51c49877d69
skyperson <skyperson@gmail.com>
2017-07-18 16:52:14 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# hook-binary-mirror
+
+![tnpm](http://web.npm.alibaba-inc.com/badge/v/hook-binary-mirror.svg)
+
+处理github上不翻墙不能下载的模块
---
+++ bin/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+/**
+ * @fileOverview
+ * @author   yanjing
+ * @since    2017/07/18
+ */
+require('../lib/index')();
